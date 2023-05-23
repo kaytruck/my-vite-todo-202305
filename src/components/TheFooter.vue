d82162c29453aa7a00d37f31564d9cb41f64822f
kaytruck <kayta.sino@gmail.com>
2023-05-23 21:29:06 +0900

    コンポーネント追加

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="footer">
+    <small>&copy;my-vite-todo-202305</small>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  margin-top: 15px;
+  height: 30px;
+}
+</style>
